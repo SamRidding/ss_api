75e67d34cee39c988fab186681b5afea72b33feb
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-10 14:18:45 +0000

    Create profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=200)
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(
+        upload_to='images/', default='../defaultuserimg_mv93dr'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
