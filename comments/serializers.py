52a5667aee8203f86f8bbdc04b10ac5c4522ea1d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-11 15:28:56 +0000

    Add comment detail serializer class to match comment with relevant track

--- comments/serializers.py
+++ comments/serializers.py
@@ -6,7 +6,9 @@ class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_img = serializers.ReadOnlyField(source='owner.profile_img.image.url')
+    profile_img = serializers.ReadOnlyField(
+        source='owner.profile_img.image.url'
+        )
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -18,3 +20,7 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'track', 'content', 'posted_at',
             'edited_at', 'is_owner', 'profile_id', 'profile_img'
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    track = serializers.ReadOnlyField(source='track.id')
