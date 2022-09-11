aaf4bd0e182aebe3a6e20308a69ec46715200af3
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-11 13:20:18 +0000

    Create comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tracks.models import Track
 
-# Create your models here.
+
+class Comment(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    content = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-posted_at']
+
+    def __str__(self):
+        return self.content
--- tracks/views.py
+++ tracks/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Track
 from .serializers import TrackSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
+from ss_api.permissions import IsOwnerOrReadOnly
 
 
 class TrackList(APIView):
