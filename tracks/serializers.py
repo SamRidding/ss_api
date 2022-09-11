b69f205baafe8820dd164eaebe1df74f68e8aadf
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-11 11:56:47 +0000

    Add track serializer for API

---
+++ tracks/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from tracks.models import Track
+
+
+class TrackSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_img = serializers.ReadOnlyField(
+        source='owner.profile_img.image.url'
+        )
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Track
+        fields = [
+            'id', 'owner', 'title', 'audio',
+            'image', 'content', 'status',
+            'posted_at', 'edited_at', 'image',
+            'is_owner', 'profile_id', 'profile_img'
+        ]
