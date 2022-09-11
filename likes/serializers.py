422697b6cd1fb3af2e0037ddffea00cbbf6ba234
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-11 16:22:48 +0000

    Add create method to inform users of duplicated likes

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from likes.models import Like
 
@@ -8,3 +9,11 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'owner', 'track', 'created_at']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicated like'
+            })
