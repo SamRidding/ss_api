af14ca9a68503e7dac2b20daae3b92ac0bda1000
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-11 16:53:16 +0000

    Create repost serializer for API

---
+++ reposts/serializers.py
@@ -0,0 +1,19 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from reposts.models import Repost
+
+
+class RespostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Repost
+        fields = ['id', 'owner', 'track', 'created_at']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicated repost'
+            })
