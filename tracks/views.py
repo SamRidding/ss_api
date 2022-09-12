d661af449cd1132cf5603aa028feab4b4c8f74b2
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-12 15:42:35 +0000

    Add filters to track views to sort by number of likes/comments

--- tracks/views.py
+++ tracks/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from ss_api.permissions import IsOwnerOrReadOnly
 from .models import Track
 from .serializers import TrackSerializer
@@ -7,7 +8,17 @@ from .serializers import TrackSerializer
 class TrackList(generics.ListCreateAPIView):
     serializer_class = TrackSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Track.objects.all()
+    queryset = Track.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-posted_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -16,4 +27,7 @@ class TrackList(generics.ListCreateAPIView):
 class TrackDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TrackSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Track.objects.all()
+    queryset = Track.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-posted_at')
