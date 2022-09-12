40af25791e5003358c14c7688d49e9b0f8abf81d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-12 15:29:41 +0000

    Add filters to profile views to sort by number of tracks/following/followers

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,32 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from ss_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        tracks_count=Count('owner__track', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'tracks_count',
+        'followers_count',
+        'following_count',
+    ]
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        tracks_count=Count('owner__track', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
     serializer_class = ProfileSerializer
