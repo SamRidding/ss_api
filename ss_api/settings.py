20bab581b6ecab2ab3c913de105023a678fa45f5
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-13 16:07:32 +0000

    Set allowed hosts

--- ss_api/settings.py
+++ ss_api/settings.py
@@ -61,7 +61,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'soundspace-api.herokuapp.com',
+    'localhost',
+]
 
 
 # Application definition
