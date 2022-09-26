83333ea5eb0bdd75e0d9615811fd29917f222637
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-26 11:43:10 +0000

    Add django filter backend to likes

--- likes/views.py
+++ likes/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from ss_api.permissions import IsOwnerOrReadOnly
 from likes.models import Like
 from likes.serializers import LikeSerializer
@@ -8,6 +9,8 @@ class LikeList(generics.ListCreateAPIView):
     serializer_class = LikeSerializer
     queryset = Like.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['owner']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
