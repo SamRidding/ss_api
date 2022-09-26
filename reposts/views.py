8a4ca8782c86a8a429111018a0acc2d6e2f9ace7
vfg-95 <sam.ridding@hotmail.co.uk>
2022-09-26 12:22:18 +0000

    Add filters to repost view

--- reposts/views.py
+++ reposts/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from ss_api.permissions import IsOwnerOrReadOnly
 from reposts.models import Repost
 from reposts.serializers import RepostSerializer
@@ -8,6 +9,8 @@ class RepostList(generics.ListCreateAPIView):
     serializer_class = RepostSerializer
     queryset = Repost.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['owner']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
